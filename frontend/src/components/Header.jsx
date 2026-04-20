7f33172e8c484bb6084c3a56ac37aa18a54b4cbf
iswwimm <kursant2000@gmail.com>
2026-04-20 20:02:31 +0200

    feat: enhance header responsiveness with dynamic height and width adjustments

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,13 +3,14 @@ import logoImg from '../assets/logo-header.png';
 
 function Header() {
   return (
-    <header className="w-full h-[125px] bg-white flex items-center justify-center relative z-50">
+    <header className="w-full h-[clamp(65px,7vw+38px,125px)] bg-white flex items-center justify-center z-50">
+      
       <div className="w-full max-w-[1280px] h-full flex items-center justify-center px-4 md:px-8 relative">
         
         <img
           src={logoImg}
           alt="Insomnia Course Logo"
-          className="w-[280px] md:w-[400px] lg:w-[540px] h-auto object-contain"
+          className="w-[clamp(225px,35vw+88px,536px)] h-auto object-contain"
           fetchpriority="high" 
         />
         
